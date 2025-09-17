ca74e86aec890cbc2b9c19f59a67fceab6b1830e
Talha-SE <rtalha.online@gmail.com>
2025-09-17 22:46:58 +0500

    Added Premium Plans v1.0.3

--- src/services/mistralService.js
+++ src/services/mistralService.js
@@ -412,8 +412,8 @@ const postMistralWithRetry = async (payload, maxRetries = 5, apiKey = MISTRAL_AP
 const { MISTRAL_API_KEY, AUTO_DETECT_LANGUAGE } = require('../utils/constants');
 
 const mistralAPIUrl = 'https://api.mistral.ai/v1/chat/completions';
-//const TRANSLATION_MODEL = 'mistral-small-2501';
-const TRANSLATION_MODEL = 'mistral-small-2409';
+const TRANSLATION_MODEL = 'mistral-small-2501';
+//const TRANSLATION_MODEL = 'mistral-small-2409';
 //const TRANSLATION_MODEL = 'mistral-medium-latest';
 /**
  * Detects the language of a given text
